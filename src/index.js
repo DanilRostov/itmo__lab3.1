6ba1417b1eb0420ea984feb8584539ae80fe8990
DanilRostov <daniil.rossy@gmail.com>
2018-03-22 22:50:35 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ let authorWrap = document.createElement( 'h4' );
 authorWrap.id = 'author';
 authorWrap.title = 'GossJS';
 let author = document.body.appendChild( authorWrap );
-
 author.textContent = `${ student.name }`;
 
 // Component Pre
 let el = document.body.appendChild( document.createElement( 'pre' ) );
-
-el.textContent += `${ moment().format( "MMM Do YY" ) } \n${ internet.email() } \n`;
+el.textContent += `Date today: ${ moment().format( "MMM Do YY" ) } \nRandom email: ${ internet.email() } \n`;
 
 
